70fd9e08be4c1910bb2fd8e13450e36d2df6eb50
Sherile <sherilecheptoo1@gmail.com>
2024-02-23 01:46:13 +0300

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-import './Contact.css';
+import "./Contact.css";
+import { IoLogoGithub } from "react-icons/io";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Contact() {
   return (
-    <div>
-      <div id="contact">
+    <div id="contact">
+      <div className="contact-info">
+        <h1>Contact Information</h1>
+        <p>
+          <span>Email:</span>{" "}
+          <a href="mailto:sherilecheptoo@outlook.com">
+            sherilecheptoo@outlook.com
+          </a>
+        </p>
+        <p>
+          <span>Phone:</span> <a href="tel:+254793721334"> +254 793 721 334 </a>
+        </p>
+        <div className="social">
+          <a href="https://github.com/" target="_blank" rel="noreferrer">
+            <IoLogoGithub />
+          </a>
+          <a href="https://shorturl.at/gwZ67" target="_blank" rel="noreferrer">
+            <FaLinkedinIn />
+          </a>
+        </div>
+      </div>
+      <div className="contact">
         <h1 className="title">Contact Me</h1>
         <div className="contact">
           <div className="form">
@@ -14,12 +36,6 @@ function Contact() {
               <textarea placeholder="Message"></textarea>
               <button type="submit">Send</button>
             </form>
-          </div>
-          <div className="contact-info">
-            <h1>Contact Information</h1>
-            <p>
-              <span>Email:</span> sherilecheptoo@outlook.com
-            </p>
           </div>
         </div>
       </div>
