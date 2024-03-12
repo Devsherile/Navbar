90abd55e238495fb9b2a8bcce58e03919c4ab1b0
Sherile <sherilecheptoo1@gmail.com>
2024-03-13 01:19:12 +0300

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,15 +6,25 @@ import { FaLinkedinIn } from "react-icons/fa";
 function Contact() {
   return (
     <div id="contact">
+      <div className="contact">
+        <h1 className="title">Contact Me</h1>
+        <div className="form">
+          <form action="submit">
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <textarea placeholder="Message"></textarea>
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
       <div className="contact-info">
-        <h1>Contact Information</h1>
-        <p>
+        <p className="email">
           <span>Email:</span>{" "}
-          <a href="mailto:sherilecheptoo@outlook.com">
-            sherilecheptoo@outlook.com
+          <a href="mailto:cheptoosherile@gmail.com">
+            cheptoosherile@gmail.com
           </a>
         </p>
-        <p>
+        <p className="phone">
           <span>Phone:</span>{" "}
           <a href="tel:+254 793 721 334"> +254 793 721 334 </a>
         </p>
@@ -25,17 +35,6 @@ function Contact() {
           <a href="https://shorturl.at/gwZ67" target="_blank" rel="noreferrer">
             <FaLinkedinIn />
           </a>
-        </div>
-      </div>
-      <div className="contact">
-        <h1 className="title">Contact Me</h1>
-        <div className="form">
-          <form action="submit">
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <textarea placeholder="Message"></textarea>
-            <button type="submit">Send</button>
-          </form>
         </div>
       </div>
     </div>
